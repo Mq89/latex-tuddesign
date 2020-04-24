836592072dc8cbae096f7f93a416e1043540311d
Markus Heinrich <mh@0x25.net>
2020-04-24 14:20:23 +0200

    move fontawesomesymbols as well

--- install_fontawesome.sh
+++ install_fontawesome.sh
@@ -4,5 +4,6 @@ wget http://mirrors.ctan.org/fonts/fontawesome.zip
 unzip fontawesome.zip
 mv fontawesome/opentype/FontAwesome.otf /usr/share/texlive/texmf-dist/fonts/opentype/public/fontawesome/FontAwesome.otf
 mv fontawesome/tex/fontawesome.sty /usr/share/texlive/texmf-dist/tex/latex/fontawesome/fontawesome.sty
+mv fontawesome/tex/*.tex /usr/share/texlive/texmf-dist/tex/latex/fontawesome/
 
 rm -r fontawesome.zip fontawesome
